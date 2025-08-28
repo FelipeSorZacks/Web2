dda2b394ba64ef97db8a845baf40ede3640b68f4
Felipe Soriano Zacarias <felipesorianozacarias@gmail.com>
2025-08-27 20:59:19 -0600

    Subido desde la Consola
    Pequeña pagina para mostrar informacion con ayuda de alertas, .html mas .js funcionales

---
+++ Tarea_21082025/App.js
@@ -0,0 +1,15 @@
+function miEdad() {
+    alert("Tengo 21 años");
+}
+
+function miPeso() {
+    alert("Mi peso es de 67kg");
+}
+
+function miEstatura() {
+    alert("Mido 1.62 m");
+}
+
+function yoDeboMaterias() {
+    alert("No debo materias en la carrera");
+}
