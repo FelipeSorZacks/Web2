a63262eaedce993bc531e42ad483ce1425bc4cfa
Felipe Soriano Zacarias <felipesorianozacarias@gmail.com>
2025-08-30 18:04:01 -0600

    subido desde la consola
    clase_26082025 operaciones en .js funcionando

---
+++ Clase_26082025/app.js
@@ -0,0 +1,13 @@
+// var nombre;
+// nombre = "Felipe";
+// console.log(nombre);
+
+// var dato = "UNAM";
+// console.log(dato);
+
+// var dato = 64;
+// console.log(dato);
+
+let nombre;
+nombre = "Felipe";
+console.log(nombre);
---
+++ Clase_26082025/app2.js
@@ -0,0 +1,36 @@
+// let x = 25;
+// let y = 64;
+// let resultado = x + y;
+// console.log("El resultado es: " + resultado);
+
+// let nombreUsuario;
+// let edadUsuario;
+// let nacionalidadUsuario;
+// let numeroCuenta;
+// let estatura;
+
+// nombreUsuario = "Felipe";
+// edadUsuario = 21;
+// nacionalidadUsuario = "Mexicana";
+// numeroCuenta = 320256444;
+// estatura = 1.62;
+
+// console.log("El nombre del usuario es: " + nombreUsuario);
+// console.log("La edad del usuario es: " + edadUsuario);  
+// console.log("La nacionalidad del usuario es: " +  nacionalidadUsuario);
+// console.log("El numero de cuenta del usuario es: " + numeroCuenta);
+// console.log("La estatura del usuario es: " +  estatura + " mts");
+
+// let saludo1 = "Hola ";
+// let saludo2 = "¿como estas?";
+// let saludoCompleto = saludo1 + " " + nombreUsuario + ", " + saludo2;
+// console.log(saludoCompleto);
+
+// let nombre = prompt("Ingresa tu nombre: ");    
+// console.log("El nombre del usuario es: " + nombre);
+// alert("Hola " + nombre + ", bienvenido a la clase de JavaScript");
+
+let numero1 = parseInt(prompt("Ingresa un numero: "));
+let numero2 = parseInt(prompt("Ingresa otro numero: "));
+let resultado = numero1 + numero2;
+console.log("El resultado de la suma es: " + resultado);
